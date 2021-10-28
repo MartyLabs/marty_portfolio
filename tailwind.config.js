549c0a7b0a0f0a3fd82ae95c0b4a4d86854876a7
UniqueDevStorm <me@devstorm.kr>
2021-10-28 19:38:45 +0900

    feat: Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    backgroundColor: (theme) => ({
+      main: "#232027"
+    }),
+    gradientColorStops: (theme) => ({
+      start: "#A2CAFF",
+      end: "#006FFF"
+    })
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
